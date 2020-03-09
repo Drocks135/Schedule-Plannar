b28a6e20f9b48aaf27dd0337e0b2e7cab32dbcf2
Gabriel Gunnink <gunninkg@mail.gvsu.edu>
2020-03-08 22:05:30 -0400

    added methods

--- GUI/MainGUI.java
+++ GUI/MainGUI.java
@@ -2,13 +2,10 @@ package GUI;
 
 
 import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
-import java.util.Date;
+import java.io.IOException;
+import java.nio.file.*;
 
 public class MainGUI extends Application {
 
@@ -21,5 +18,52 @@ public class MainGUI extends Application {
     @Override
     public void start(Stage stage) {
         monthView.display();
+    }
+
+    public static String readFileAsString(String fileName) throws IOException {
+        String data = "";
+        data = new String(Files.readAllBytes(Paths.get(fileName)));
+        return data;
+    }
+
+    public void save(EventPackage[] event) {
+        //TODO
+        // create/overwrite file:
+        // create string: check
+        // loop to create a string of events: check
+        // save string to file:
+        String eventString = "";
+        for (int i = 0; i < event.length; i++) {
+            eventString += toString(event[i]);
+        }
+    }
+
+    public String toString(EventPackage event) {
+        String eventString = "";
+        eventString += (event.getEventName() + ",");
+        eventString += (event.getEventDate() + ",");
+        eventString += (event.getEventContactName() + ";");
+        return eventString;
+    }
+
+    public EventPackage stringToEvent(String event) {
+        EventPackage evnt = new EventPackage();
+        // Splits events into an array of Strings.
+        String[] temp = event.split(",");
+        evnt.setEventName(temp[0]);
+        evnt.setEventDate(temp[1]);
+        evnt.setEventContactName(temp[2]);
+        return evnt;
+    }
+
+    public EventPackage[] load() throws IOException {
+        // We could use any other way to load a file i just threw this one here for testing
+        String data = readFileAsString("C:\\Users\\lard2\\IdeaProjects\\Schedule-Plannar-master\\save_load.txt");
+        String[] events = data.split(";"); // split file into different events
+        EventPackage[] eventPacs = new EventPackage[events.length];
+        for (int i = 0; i < events.length; i++) {
+            eventPacs[i] = stringToEvent(events[i]);
+        }
+        return eventPacs;
     }
 }
