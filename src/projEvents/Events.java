6cae39c66ca890750dd9b29e88d50ac32bac46e7
Dalton <claybaud@mail.gvsu.edu>
2020-04-17 21:26:37 -0400

    Revised comments

--- src/projEvents/Events.java
+++ src/projEvents/Events.java
@@ -5,15 +5,14 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 
-/**************************************************************************************************
+/******************************************************************************
  @author: Ayden Martin
  @Version: 1.0
-  * Events is a class that will be the default object that is created for the schedule planner,
-  * All other objects that are created for the calander will be an implementation of Events
-  * Thus all calander objects will have a
+  * Events is a class that will be the default object that is created for the
+  * schedule planner, All other objects that are created for the calander will
+  * be an implementation of Events, thus all calander objects will have a
   * Name, Details, and Date attached to them
- *************************************************************************************************/
-
+ *****************************************************************************/
 public class Events {
 
     private String name=null;
@@ -22,45 +21,48 @@ public class Events {
     
     public Events() { }
 
-    /**********************************************************************************************
+    /**************************************************************************
      * @param
      * name: The name of the event
      * details: What is the event about
      * due: When is the vent happening
      * @return none
-     *********************************************************************************************/
+     *************************************************************************/
     public Events(String name, String details, LocalDate due) {
         setDetails(details);
         setName(name);
         setDue(due);
     }
 
-     /*********************************************************************************************
+     /*************************************************************************
+      * Gets the date the the event will take place
      * @return due: This is when the Event is going to happen
-     */
+     *************************************************************************/
     public LocalDate getDue() {
         return this.due;
     }
 
-    /********************************************************
-     * @return The details of an event
-     ********************************************************/
+    /**************************************************************************
+     * Gets the details of the event
+     * @return A String representing the details of an event
+     *************************************************************************/
     public String getDetails() {
         return this.details;
     }
 
-    /*****************************************************
+    /**************************************************************************
+     * Gets the name of the event
      * @return Name: The name of an event
-     *****************************************************/
+     *************************************************************************/
     public String getName() {
         return this.name;
     }
 
 
-    /***************************************************************
+    /**************************************************************************
      * Sets the details value of an Event
      * @param details: A string describing the details of an event
-     ***************************************************************/
+     *************************************************************************/
     public void setDetails(String details) {
         Errors e = Errors.getInstance();
         if(details.length() > 50){
@@ -70,18 +72,18 @@ public class Events {
             this.details = details;
     }
 
-    /**
+    /**************************************************************************
      * Sets the date that an event will be put on the calendar
      * @param due: A LocalDate of the day an event will take place on
-     */
+     *************************************************************************/
     public void setDue(LocalDate due) {
             this.due = due;
     }
 
-    /**
+    /**************************************************************************
      * Sets the name of an event
      * @param name: A String that is the name of the event
-     */
+     *************************************************************************/
     public void setName(String name) {
         Errors e = Errors.getInstance();
         if(name.length() > 50){
@@ -108,11 +110,12 @@ public class Events {
     }
 
     @Override
-    /**********************************************************************************************
+    /**************************************************************************
      * Compares to Events objects and returns if their members are equal
      * @param o: Any object to compare to an Events
-     * @return returns true if every field in the objects are equal returns false else
-     **********************************************************************************************/
+     * @return returns true if every field in the objects are equal returns
+     * false else
+     *************************************************************************/
     public boolean equals(Object o) {
         if (o == this) {
             return true;
